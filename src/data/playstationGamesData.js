df481fc44cd8ccab8550aa6160ae3df8787cea78
Nathan <nathanbenson.affiliate@gmail.com>
2021-05-15 21:19:30 +0200

    added playstation 5 game data

---
+++ src/data/playstationGamesData.js
@@ -0,0 +1,41 @@
+import horizon from '../home/images/horizon.jepg';
+import spiderman from './playstationGamesImages/spiderman.jpg';
+import cod from './playstationGamesImages/cod.jpeg';
+import valhalla from './playstationGamesImages/valhalla.jpeg';
+
+const playStationGamesData = [
+    {
+        name: 'Horizon: Forbidden West',
+        image: horizon,
+        shortDescription: 'Experience Aloy of the Nora’s entire legendary quest to unravel the mysteries of a world ruled by deadly Machines',
+        console: 'Playstation 5',
+        new: true
+
+    },
+    {
+        name: 'Spiderman: Miles Morales',
+        image: spiderman,
+        shortDescription: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+        console: 'Playstation 5',
+        new: true
+
+    },
+    {
+        name: 'COD: Cold war',
+        image: cod,
+        shortDescription: 'The iconic black Ops series is back with Call of Duty: Black Ops cold war - the direct sequel to the original and fan-favorite Call of Duty: Black Ops',
+        console: 'Playstation 5',
+        new: true
+
+    },
+    {
+        name: `Assasin's creed: Valhalla`,
+        image: valhalla,
+        shortDescription: `You are Eivor, a fierce Viking warrior raised on Tales of battle and glory. Explore a mysterious and beautiful open world set against the brutal backdrop of England's dark ages`,
+        console: 'Playstation 5',
+        new: true
+
+    },
+
+
+]
